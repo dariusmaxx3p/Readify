f23e22097bf687681fe1b61980b840d78d611d45
HungTP-Play <hungtp.play@gmail.com>
2024-12-21 16:02:10 +0700

    feat: add pwa

---
+++ app/manifest.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Readify.",
+    short_name: "rdf.",
+    description: "Book sharing and review platform.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/android-chrome-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      // Apple touch icon
+      {
+        src: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  };
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextConfig } from "next";
+import nextPWA from "next-pwa";
+
+const withPWA = nextPWA({
+  dest: "public",
+  // Disable PWA in development mode
+  disable: process.env.NODE_ENV === "development",
+  // Additional PWA options
+  register: true,
+  skipWaiting: true,
+  // Optionally, customize caching strategies
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+        },
+      },
+    },
+    // Add more caching strategies as needed
+  ],
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig as any);
